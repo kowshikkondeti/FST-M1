7c75f217db2547d783ac6d87701a5f0967a592f0
kowshikkondeti <60208847+kowshikkondeti@users.noreply.github.com>
2022-04-29 00:41:02 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript.sql
@@ -0,0 +1,20 @@
+REM   Script: Activity1
+REM   Creation of table
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
